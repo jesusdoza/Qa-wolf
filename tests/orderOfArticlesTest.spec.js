91897c375303938235d7d0651a93bf07d42c5ac0
jesus doza <jesus.doza@gmail.com>
2024-09-05 19:33:11 -0500

    add test description

--- tests/orderOfArticlesTest.spec.js
+++ tests/orderOfArticlesTest.spec.js
@@ -2,4 +2,7 @@ const { chromium } = require("playwright");
 const { expect, test } = require("playwright/test");
 const { sortHackerNewsArticles } = require("../index.js");
 
-test("newest articles page", sortHackerNewsArticles);
+test(
+  "newest articles page should be in order from newest to oldest",
+  sortHackerNewsArticles
+);
